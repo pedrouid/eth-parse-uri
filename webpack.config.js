e0521b64d765cfdc5f73f2a2956b79e52f02c922
Pedro Gomes <pedrogomes94@gmail.com>
2018-09-19 11:47:07 -0700

    v0.6.6

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index.js',
     library: 'eth-parse-uri',
-    libraryTarget: 'umd'
+    libraryTarget: 'commonjs2'
   },
   module: {
     rules: [
